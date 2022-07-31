c0c9ac5522c00cab2c1942f60ca2fe51a3a2bd00
CharlesXvr <thestuntman92@gmail.com>
2022-07-31 19:47:29 -0300

    controllers set to conect with angular

--- src/main/java/com/portfolio/backend/controllers/DatasourceConfig.java
+++ src/main/java/com/portfolio/backend/controllers/DatasourceConfig.java
@@ -12,7 +12,7 @@ public class DatasourceConfig {
     public DataSource datasource() {
         return DataSourceBuilder.create()
                 .driverClassName("com.mysql.cj.jdbc.Driver")
-                .url("mysql://ud15hwwwyxcqgjzm:zMfVsiomvCyu959V0f0W@bqkib7hnrvu7ir8slrbc-mysql.services.clever-cloud.com:3306/bqkib7hnrvu7ir8slrbc")
+                .url("jdbc:mysql://bqkib7hnrvu7ir8slrbc-mysql.services.clever-cloud.com:3306/bqkib7hnrvu7ir8slrbc")
                 .username("zMfVsiomvCyu959V0f0W")
                 .password("ud15hwwwyxcqgjzm")
                 .build();
