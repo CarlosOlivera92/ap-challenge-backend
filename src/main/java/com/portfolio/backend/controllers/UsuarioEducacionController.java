ca0b2728ead47f27c8b206760dda5d078c0190bc
CharlesXvr <thestuntman92@gmail.com>
2022-08-01 00:21:38 -0300

    cors

--- src/main/java/com/portfolio/backend/controllers/UsuarioEducacionController.java
+++ src/main/java/com/portfolio/backend/controllers/UsuarioEducacionController.java
@@ -9,7 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin(value = "*")
+
+@CrossOrigin(value = "/**")
 @RestController
 @RequestMapping("/auth/userinstitutes")
 public class UsuarioEducacionController {
