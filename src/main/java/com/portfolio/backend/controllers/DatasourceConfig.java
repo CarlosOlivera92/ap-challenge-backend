83a3f09a0e466969165180639b6272cee0513e28
CharlesXvr <thestuntman92@gmail.com>
2022-07-31 21:04:15 -0300

    controllers set to conect with angular

--- src/main/java/com/portfolio/backend/controllers/DatasourceConfig.java
+++ src/main/java/com/portfolio/backend/controllers/DatasourceConfig.java
@@ -1,16 +1,18 @@
 package com.portfolio.backend.controllers;
 
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
 public class DatasourceConfig {
-    /*
     @Bean
     public DataSource datasource() {
         return DataSourceBuilder.create()
                 .driverClassName("com.mysql.cj.jdbc.Driver")
-                .url("jdbc:mysql://bqkib7hnrvu7ir8slrbc-mysql.services.clever-cloud.com:3306/bqkib7hnrvu7ir8slrbc")
+                .url("jdbc:mysql://ud15hwwwyxcqgjzm:zMfVsiomvCyu959V0f0W@bqkib7hnrvu7ir8slrbc-mysql.services.clever-cloud.com:3306/bqkib7hnrvu7ir8slrbc")
                 .username("zMfVsiomvCyu959V0f0W")
                 .password("ud15hwwwyxcqgjzm")
                 .build();
     }
-
-     */
 }
